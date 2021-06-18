cc9d217608e446eed1617177429b3241e46c8284
Kyle <kylejsimko@gmail.com>
2021-06-17 22:07:54 -0400

    typo

--- python/main.py
+++ python/main.py
@@ -38,9 +38,9 @@ binanceExchange = ccxt.binance({
     'secret': os.environ.get('binance_secret'),
     'enableRateLimit': True,
     'options': {
-        # 'defaultType': 'future', # USD-M
+         'defaultType': 'future', # USD-M
         #'defaultType': 'delivery', # COIN-M
-        'defaultType': 'spot', # COIN-M
+        #'defaultType': 'spot', # COIN-M
         'leverageBrackets': None,
     },
 })
@@ -59,12 +59,12 @@ def test_exchange_methods(exchange):
     #print('##########\nfetch_balance\n##########')
     #pprint(exchange.fetch_balance())
     #print('##########\nfetch_positions\n##########')
-    #pprint(exchange.fetch_positions())
+    pprint(exchange.fetch_positions())
     #print(exchange.fetch_positions(None, {'currency': 'BTC'})) # phemex - make ts call that calls all
     #pprint(exchange.fetch_positions(None, {'type': 'all'})) # bybit
     #print('##########\nfetch_orders\n##########')
     #pprint(exchange.fetch_orders())
-    pprint(exchange.fetch_orders('COCOS/USDT', None, None)) # phemex /  bybit
+    #pprint(exchange.fetch_orders('COCOS/USDT', None, None)) # phemex /  bybit
     #print('##########\nfetch_my_trades\n##########')
     #pprint(exchange.fetch_my_trades())
     #pprint(exchange.fetch_my_trades('DOGE/USD', None, None))
