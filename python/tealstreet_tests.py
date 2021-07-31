21c0f71faeb27151ad69a3055eb93e0ca9134b58
Kyle <kylejsimko@gmail.com>
2021-07-30 21:04:51 -0400

    pytest POST testing for create_order

--- python/tealstreet_tests.py
+++ python/tealstreet_tests.py
@@ -64,60 +64,448 @@ okexExchange = ccxt.okex({
         'x-simulated-trading': '1',
     },
 })
-__test__ = True
 okexExchange.set_sandbox_mode(True)
 
-orders = []
+__test__ = True
+
+# start test stuff
+
 exchange = bybitExchange
-
-def do_create_order(args):
-    print('༼ つ ◕_◕ ༽つ =========================')
-    print('create_order(', *args, ')')
-    result = exchange.create_order(*args)
-    return result
-
 
 symbol = 'BTC/USD'
 size = 1
 ticker = exchange.fetch_ticker(symbol)
 last = ticker['last']
 
-print(exchange.name)
+orders = []
+actions = []
 
-print("Market buy, post only = true.")
-result = do_create_order([
-    symbol,
-    'market',
-    'buy',
-    size,
-    None,
-    {
-        'stopPrice': None,
-        'timeInForce': 'PO', # GTC, PO
-        'reduceOnly': False,
-        'trigger': None,
-        'closeOnTrigger': None,
-        'basePrice': None
-    }
-])
-order = exchange.fetch_order(result['id'], symbol)
-assert result['postOnly'] == True
-#
-# print("Limit buy below last, reduce only = false and post only = false.")
-# result = test_create_order(exchange, [
-#     symbol,
-#     'limit',
-#     'buy',
-#     size,
-#     last * 0.95,
-#     {
-#         'stopPrice': None,
-#         'timeInForce': 'GTC',  # GTC, PO
-#         'reduceOnly': False,
-#         'trigger': None,
-#         'closeOnTrigger': None,
-#         'basePrice': None
-#     }
-# ])
-# order = exchange.fetch_order(result['id'], symbol)
-# assert result['postOnly'] == True and result['reduceOnly']
+def get_close_on_trigger_value(result):
+    keys = ['close_on_trigger']
+    for key in keys:
+        if key in result:
+            return exchange.safe_value(result, key)
+    return None
+
+def get_info_trigger_value(result):
+    keys = ['trigger', 'trigger_by']
+    for key in keys:
+        if key in result:
+            return exchange.safe_value(result, key)
+    return None
+
+def do_create_order(args):
+    print('create_order(', *args, ')')
+    result = exchange.create_order(*args)
+    print("Result:")
+    pprint(result)
+    print('༼ つ ◕_◕ ༽つ')
+    return result
+
+def test_market_buy_post_only():
+    print(exchange.name)
+    test_size = size * 2
+    # action = 'Market buy {size*2} {symbol}. Post only. Position size '
+    # actions.append(action)
+    print("Market buy, post only = true.")
+    result = do_create_order([
+        symbol,
+        'market',
+        'buy',
+        test_size,
+        None,
+        {
+            'stopPrice': None,
+            'timeInForce': 'PO', # GTC, PO
+            'reduceOnly': False,
+            'trigger': None,
+            'closeOnTrigger': None,
+            'basePrice': None
+        }
+    ])
+    assert result['postOnly'] == True or result['timeInForce'] in ['IOC', 'PO']
+
+def test_market_buy_not_post_only():
+    print(exchange.name)
+    print("Market buy, post only = false.")
+    result = do_create_order([
+        symbol,
+        'market',
+        'buy',
+        size*2,
+        None,
+        {
+            'stopPrice': None,
+            'timeInForce': 'GTC', # GTC, PO
+            'reduceOnly': False,
+            'trigger': None,
+            'closeOnTrigger': None,
+            'basePrice': None
+        }
+    ])
+    assert result['postOnly'] == False
+
+def test_market_sell_post_only():
+    print(exchange.name)
+    print("Market buy, post only = true.")
+    result = do_create_order([
+        symbol,
+        'market',
+        'sell',
+        size,
+        None,
+        {
+            'stopPrice': None,
+            'timeInForce': 'PO', # GTC, PO
+            'reduceOnly': False,
+            'trigger': None,
+            'closeOnTrigger': None,
+            'basePrice': None
+        }
+    ])
+    assert result['postOnly'] == True or result['timeInForce'] in ['IOC', 'PO']
+
+def test_market_sell_not_post_only():
+    print(exchange.name)
+    print("Market buy, post only = false.")
+    result = do_create_order([
+        symbol,
+        'market',
+        'sell',
+        size,
+        None,
+        {
+            'stopPrice': None,
+            'timeInForce': 'GTC', # GTC, PO
+            'reduceOnly': False,
+            'trigger': None,
+            'closeOnTrigger': None,
+            'basePrice': None
+        }
+    ])
+    assert result['postOnly'] == False
+
+def test_limit_buy_below_last_post_only_reduce_only():
+    print("Limit buy below last, reduce only = true and post only = true. Should not post.")
+    try:
+        result = do_create_order([
+            symbol,
+            'limit',
+            'buy',
+            size,
+            last * 0.99,
+            {
+                'stopPrice': None,
+                'timeInForce': 'PO',  # GTC, PO
+                'reduceOnly': True,
+                'trigger': None,
+                'closeOnTrigger': None,
+                'basePrice': None
+            }
+        ])
+        assert False
+    except:
+        assert True
+
+def test_limit_buy_below_last_not_post_only_not_reduce_only():
+    print("Limit buy below last, reduce only = false and post only = false.")
+    result = do_create_order([
+        symbol,
+        'limit',
+        'buy',
+        size,
+        last * 0.98,
+        {
+            'stopPrice': None,
+            'timeInForce': 'GTC',  # GTC, PO
+            'reduceOnly': False,
+            'trigger': None,
+            'closeOnTrigger': None,
+            'basePrice': None
+        }
+    ])
+    assert result['postOnly'] == False
+
+def test_limit_sell_below_last_post_only_reduce_only():
+    print("Limit sell above last, reduce only = true and post only = true.")
+    result = do_create_order([
+        symbol,
+        'limit',
+        'sell',
+        size,
+        last * 1.01,
+        {
+            'stopPrice': None,
+            'timeInForce': 'PO', # GTC, PO
+            'reduceOnly': True,
+            'trigger': None,
+            'closeOnTrigger': None,
+            'basePrice': None
+        }
+    ])
+    assert result['postOnly'] == True # TODO: reduce only
+
+def test_limit_sell_above_last_not_post_only_not_reduce_only():
+    print("Limit sell above last, reduce only = false and post only = false.")
+    result = do_create_order([
+        symbol,
+        'limit',
+        'sell',
+        size,
+        last * 1.02,
+        {
+            'stopPrice': None,
+            'timeInForce': 'GTC',  # GTC, PO
+            'reduceOnly': False,
+            'trigger': None,
+            'closeOnTrigger': None,
+            'basePrice': None
+        }
+    ])
+    assert result['postOnly'] == False
+
+def test_stop_market_sell_below_last_close_on_trigger():
+    print("Market sell stop below last. Close on trigger = true.")
+    result = do_create_order([
+       symbol,
+       'stop',
+       'sell',
+       size,
+       None,
+       {
+          'stopPrice':last * 0.97,
+          'timeInForce':'GTC',
+          'reduceOnly':None,
+          'trigger':'Last',
+          'closeOnTrigger':True,
+          'basePrice': last
+       }
+    ])
+    assert get_close_on_trigger_value(result['info']) == True or get_close_on_trigger_value(result['info']) == None
+
+def test_stop_market_sell_below_last():
+    print("Market sell stop below last. Close on trigger = false.")
+    result = do_create_order([
+        symbol,
+        'stop',
+        'sell',
+        size,
+        None,
+        {
+            'stopPrice': last * 0.96,
+            'timeInForce': 'GTC',
+            'reduceOnly': None,
+            'trigger': 'Last',
+            'closeOnTrigger': False,
+            'basePrice':  last
+        }
+    ])
+    assert get_close_on_trigger_value(result['info']) == False or get_close_on_trigger_value(result['info']) == None
+
+def test_stop_market_sell_above_last():
+    print("Market sell stop above last. Close on trigger = false.")
+    result = do_create_order([
+        symbol,
+        'stop',
+        'sell',
+        size,
+        None,
+        {
+            'stopPrice': last * 1.03,
+            'timeInForce': 'GTC',
+            'reduceOnly': None,
+            'trigger': 'Last',
+            'closeOnTrigger': False,
+            'basePrice': last
+        }
+    ])
+    assert get_close_on_trigger_value(result['info']) == False or get_close_on_trigger_value(result['info']) == None
+
+def test_stop_market_buy_above_last_close_on_trigger():
+    print("Market buy stop above last. Close on trigger = true.")
+    result = do_create_order([
+       symbol,
+       'stop',
+       'buy',
+       size,
+       None,
+       {
+          'stopPrice':last * 1.04,
+          'timeInForce':'GTC',
+          'reduceOnly':None,
+          'trigger':'Last',
+          'closeOnTrigger':True,
+          'basePrice': last
+       }
+    ])
+    assert get_close_on_trigger_value(result['info']) == True or get_close_on_trigger_value(result['info']) == None
+
+def test_stop_market_buy_above_last():
+    print("Market buy stop above last. Close on trigger = false.")
+    result = do_create_order([
+        symbol,
+        'stop',
+        'buy',
+        size,
+        None,
+        {
+            'stopPrice': last * 1.05,
+            'timeInForce': 'GTC',
+            'reduceOnly': None,
+            'trigger': 'Last',
+            'closeOnTrigger': False,
+            'basePrice': last
+        }
+    ])
+    assert get_close_on_trigger_value(result['info']) == False or get_close_on_trigger_value(result['info']) == None
+
+def test_stop_market_buy_below_last():
+    print("Market buy stop below last. Close on trigger = false.")
+    result = do_create_order([
+       symbol,
+        'stop',
+        'buy',
+        size,
+        None,
+        {
+            'stopPrice': last * 0.95,
+            'timeInForce': 'GTC',
+            'reduceOnly': None,
+            'trigger': 'Last',
+            'closeOnTrigger': False,
+            'basePrice': last
+        }
+    ])
+    assert get_close_on_trigger_value(result['info']) == False or get_close_on_trigger_value(result['info']) == None
+
+
+
+
+
+
+
+
+
+def test_stop_limit_buy_above_last():
+    print("Limit buy stop above last. Close on trigger = false. Trigger = 'Last'.")
+    result = do_create_order([
+        symbol,
+        'stoplimit',
+        'buy',
+        size,
+        last * 0.94,
+        {
+            'stopPrice': last * 1.06,
+            'timeInForce': 'PO',
+            'reduceOnly': None,
+            'trigger': 'Last',
+            'closeOnTrigger': False,
+            'basePrice': last
+        }
+    ])
+    assert 'Last' in get_info_trigger_value(result['info']) \
+           and (get_close_on_trigger_value(result['info']) == True or get_close_on_trigger_value(result['info']) == None)
+
+def test_stop_limit_buy_above_mark():
+    print("Limit buy stop above last. Close on trigger = true. Trigger = 'Mark'.")
+    result = do_create_order([
+        symbol,
+        'stoplimit',
+        'buy',
+        size,
+        last * 0.93,
+        {
+            'stopPrice': last * 1.07,
+            'timeInForce': 'PO',
+            'reduceOnly': None,
+            'trigger': 'Mark',
+            'closeOnTrigger': True,
+            'basePrice': last
+        }
+    ])
+    assert 'Mark' in get_info_trigger_value(result['info']) \
+           and (get_close_on_trigger_value(result['info']) == True or get_close_on_trigger_value(result['info']) == None)
+
+def test_stop_limit_buy_above_index():
+    print("Limit buy stop above last. Close on trigger = true. Trigger = 'Index'.")
+    result = do_create_order([
+        symbol,
+        'stoplimit',
+        'buy',
+        size,
+        last * 0.92,
+        {
+            'stopPrice': last * 1.08,
+            'timeInForce': 'PO',
+            'reduceOnly': None,
+            'trigger': 'Index',
+            'closeOnTrigger': True,
+            'basePrice': last
+        }
+    ])
+    assert 'Index' in get_info_trigger_value(result['info']) \
+           and (get_close_on_trigger_value(result['info']) == True or get_close_on_trigger_value(result['info']) == None)
+
+
+
+
+
+def test_stop_limit_sell_below_last():
+    print("Limit sell stop below last. Close on trigger = false. Trigger = 'Last'.")
+    result = do_create_order([
+        symbol,
+        'stoplimit',
+        'sell',
+        size,
+        last * 1.09,
+        {
+            'stopPrice': last * 0.91,
+            'timeInForce': 'PO',
+            'reduceOnly': None,
+            'trigger': 'Last',
+            'closeOnTrigger': False,
+            'basePrice': last
+        }
+    ])
+    assert 'Last' in get_info_trigger_value(result['info']) \
+           and (get_close_on_trigger_value(result['info']) == True or get_close_on_trigger_value(result['info']) == None)
+
+def test_stop_limit_sell_below_mark():
+    print("Limit sell stop below mark. Close on trigger = true. Trigger = 'Mark'.")
+    result = do_create_order([
+        symbol,
+        'stoplimit',
+        'sell',
+        size,
+        last * 1.10,
+        {
+            'stopPrice': last * 0.90,
+            'timeInForce': 'PO',
+            'reduceOnly': None,
+            'trigger': 'Mark',
+            'closeOnTrigger': True,
+            'basePrice': last
+        }
+    ])
+    assert 'Mark' in get_info_trigger_value(result['info']) \
+           and (get_close_on_trigger_value(result['info']) == True or get_close_on_trigger_value(result['info']) == None)
+
+def test_stop_limit_sell_below_index():
+    print("Limit sell stop below last. Close on trigger = true. Trigger = 'Index'.")
+    result = do_create_order([
+        symbol,
+        'stoplimit',
+        'sell',
+        size,
+        last * 1.11,
+        {
+            'stopPrice': last * 0.89,
+            'timeInForce': 'PO',
+            'reduceOnly': None,
+            'trigger': 'Index',
+            'closeOnTrigger': True,
+            'basePrice': last
+        }
+    ])
+    assert 'Index' in get_info_trigger_value(result['info']) \
+           and (get_close_on_trigger_value(result['info']) == True or get_close_on_trigger_value(result['info']) == None)
