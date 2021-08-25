1af03ab2532985ad253cf92e092c417d56a1e750
justiceorjustus <kylejsimko@gmail.com>
2021-08-25 09:58:58 -0400

    bybit watchTicker update round 2

--- js/bybit.js
+++ js/bybit.js
@@ -582,9 +582,9 @@ module.exports = class bybit extends Exchange {
         const timestamp = undefined;
         const marketId = this.safeString (ticker, 'symbol');
         const symbol = this.safeSymbol (marketId, market);
-        const last = this.safeNumber (ticker, 'last_price') || new Precise (this.safeString (ticker, 'last_price_e4'), '1e4');
-        const open = this.safeNumber (ticker, 'prev_price_24h') || new Precise (this.safeString (ticker, 'prev_price_24h_e4'), '1e4');
-        let percentage = this.safeNumber (ticker, 'price_24h_pcnt') || new Precise (this.safeString (ticker, 'price_24h_pcnt_e6'), '1e6');
+        const last = this.safeNumber (ticker, 'last_price') || Precise.stringDiv (this.safeString (ticker, 'last_price_e4'), '1e4');
+        const open = this.safeNumber (ticker, 'prev_price_24h') || Precise.stringDiv (this.safeString (ticker, 'prev_price_24h_e4'), '1e4');
+        let percentage = this.safeNumber (ticker, 'price_24h_pcnt') || Precise.stringDiv (this.safeString (ticker, 'price_24h_pcnt_e6'), '1e6');
         if (percentage !== undefined) {
             percentage *= 100;
         }
@@ -594,25 +594,25 @@ module.exports = class bybit extends Exchange {
             change = last - open;
             average = this.sum (open, last) / 2;
         }
-        const baseVolume = this.safeNumber (ticker, 'turnover_24h') || new Precise (this.safeString (ticker, 'turnover_24h_e8'), '1e8');
-        const quoteVolume = this.safeNumber (ticker, 'volume_24h') || new Precise (this.safeString (ticker, 'volume_24h_e8'), '1e8');
+        const baseVolume = this.safeNumber (ticker, 'turnover_24h') || Precise.stringDiv (this.safeString (ticker, 'turnover_24h_e8'), '1e8');
+        const quoteVolume = this.safeNumber (ticker, 'volume_24h') || Precise.stringDiv (this.safeString (ticker, 'volume_24h_e8'), '1e8');
         const vwap = this.vwap (baseVolume, quoteVolume);
         return {
             'symbol': symbol,
             'timestamp': timestamp,
             'datetime': this.iso8601 (timestamp),
-            'high': this.safeNumber (ticker, 'high_price_24h') || new Precise (this.safeString (ticker, 'high_price_24h_e4'), '1e4'),
-            'low': this.safeNumber (ticker, 'low_price_24h') || new Precise (this.safeString (ticker, 'low_price_24h_e4'), '1e4'),
-            'bid': this.safeNumber (ticker, 'bid_price') || new Precise (this.safeString (ticker, 'bid1_price_e4'), '1e4'),
+            'high': this.safeNumber (ticker, 'high_price_24h') || Precise.stringDiv (this.safeString (ticker, 'high_price_24h_e4'), '1e4'),
+            'low': this.safeNumber (ticker, 'low_price_24h') || Precise.stringDiv (this.safeString (ticker, 'low_price_24h_e4'), '1e4'),
+            'bid': this.safeNumber (ticker, 'bid_price') || Precise.stringDiv (this.safeString (ticker, 'bid1_price_e4'), '1e4'),
             'bidVolume': undefined,
-            'ask': this.safeNumber (ticker, 'ask_price') || new Precise (this.safeString (ticker, 'ask1_price_e4'), '1e4'),
+            'ask': this.safeNumber (ticker, 'ask_price') || Precise.stringDiv (this.safeString (ticker, 'ask1_price_e4'), '1e4'),
             'askVolume': undefined,
             'vwap': vwap,
             'open': open,
             'close': last,
             'last': last,
-            'mark': this.safeNumber (ticker, 'mark_price') || new Precise (this.safeString (ticker, 'mark_price_e4'), '1e4'),
-            'index': this.safeNumber (ticker, 'index_price') || new Precise (this.safeString (ticker, 'index_price_e4'), '1e4'),
+            'mark': this.safeNumber (ticker, 'mark_price') || Precise.stringDiv (this.safeString (ticker, 'mark_price_e4'), '1e4'),
+            'index': this.safeNumber (ticker, 'index_price') || Precise.stringDiv (this.safeString (ticker, 'index_price_e4'), '1e4'),
             'previousClose': undefined,
             'change': change,
             'percentage': percentage,
