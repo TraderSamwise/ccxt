75d3e8e2c21df26f8bbe0b81306e86a35efc467b
justiceorjustus <kylejsimko@gmail.com>
2021-10-18 09:32:38 -0400

    bybit trigger UNKNOWN fix

--- python/ccxt/bybit.py
+++ python/ccxt/bybit.py
@@ -1228,7 +1228,7 @@ class bybit(Exchange):
         postOnly = (timeInForce == 'PO')
         reduce = self.safe_value(order, 'reduce_only')
         close = self.safe_value(order, 'close_on_trigger')
-        trigger = self.reverse_api_trigger_type(self.safe_string(order, 'trigger_by') or self.safe_string(order, 'sl_trigger_by') or self.safe_string(order, 'tp_trigger_by'))
+        trigger = self.reverse_api_trigger_type(self.safe_string(order, 'trigger_by') != 'UNKNOWN' and self.safe_string(order, 'trigger_by') or self.safe_string(order, 'sl_trigger_by') != 'UNKNOWN' and self.safe_string(order, 'sl_trigger_by') or self.safe_string(order, 'tp_trigger_by') != 'UNKNOWN' and self.safe_string(order, 'tp_trigger_by'))
         return self.safe_order({
             'info': order,
             'id': id,
