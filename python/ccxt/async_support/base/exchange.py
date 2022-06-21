3f5a0b56844189a499f28af1bd43309094eadfe2
Sam <sam@tealstreet.io>
2022-06-21 13:25:15 -0500

    checkpoint

--- python/ccxt/async_support/base/exchange.py
+++ python/ccxt/async_support/base/exchange.py
@@ -19,7 +19,7 @@ import yarl
 # -----------------------------------------------------------------------------
 from ccxt.async_support.base.asyncio_utils import AsyncioSafeTasks
 
-from ccxt.async_support.base.throttle import throttle
+from ccxt.async_support.base.throttler import Throttler
 
 # -----------------------------------------------------------------------------
 
@@ -125,14 +125,13 @@ class Exchange(AsyncioSafeTasks, ExchangeTealstreetMixin, BaseExchange):
         self.own_session = 'session' not in config
         self.cafile = config.get('cafile', certifi.where())
         super(Exchange, self).__init__(config)
+        self.throttle = None
         self.init_rest_rate_limiter()
         self.markets_loading = None
         self.reloading_markets = False
 
     def init_rest_rate_limiter(self):
-        self.throttle = throttle(self.extend({
-            'loop': self.asyncio_loop,
-        }, self.tokenBucket))
+        self.throttle = Throttler(self.tokenBucket, self.asyncio_loop, parent_task_manager=self)
 
     def __del__(self):
         if self.session is not None:
@@ -160,7 +159,7 @@ class Exchange(AsyncioSafeTasks, ExchangeTealstreetMixin, BaseExchange):
                 await self.session.close()
             self.session = None
 
-    async def fetch2(self, path, api='public', method='GET', params={}, headers=None, body=None):
+    async def fetch2(self, path, api='public', method='GET', params={}, headers=None, body=None, config={}, context={}):
 
         trigger_ratelimit = params.pop('triggerRatelimit', True)
 
@@ -168,8 +167,9 @@ class Exchange(AsyncioSafeTasks, ExchangeTealstreetMixin, BaseExchange):
         # TEALSTREET
         self.check_rate_limits()
 
-        if self.enableRateLimit:
-            await self.throttle(self.rateLimit)
+        # if self.enableRateLimit:
+        #     cost = self.calculate_rate_limiter_cost(api, method, path, params, config, context)
+        #     await self.throttle(cost)
         self.lastRestRequestTimestamp = self.milliseconds()
         request = self.sign(path, api, method, params, headers, body)
         try:
@@ -183,6 +183,9 @@ class Exchange(AsyncioSafeTasks, ExchangeTealstreetMixin, BaseExchange):
                     self.set_rate_limit_status(True)
                     raise RateLimitExceeded(self.id + ' ' +  json.dumps({'error': 'Account or exchange appears to be rate limited.'}))
             raise e
+
+    async def request(self, path, api='public', method='GET', params={}, headers=None, body=None, config={}, context={}):
+        return await self.fetch2(path, api, method, params, headers, body, config, context)
 
     async def fetch(self, url, method='GET', headers=None, body=None):
         """Perform a HTTP request and return decoded JSON data"""
