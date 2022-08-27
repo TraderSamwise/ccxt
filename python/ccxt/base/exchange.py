f79787447e5cbb2f83d55091ff8de1437f7b53e4
Rayana Shiba <rayana@tealstreet.io>
2022-08-27 11:29:38 +0200

    Added trailingStop to known order types (for FTX)

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -172,6 +172,7 @@ class Exchange(TealstreetMixin, object):
         'stoplimit': 'StopLimit',
         'marketiftouched': 'MarketIfTouched',
         'limitiftouched': 'LimitIfTouched',
+        'trailingstop': 'trailingStop',
     }
     reverseOrderTypes = {
         'market': 'Market',
